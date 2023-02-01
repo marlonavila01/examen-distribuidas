de47695246fa7ab76066aa3774afd75977f02625
marlonavila01 <85771524+marlonavila01@users.noreply.github.com>
2023-02-01 17:29:45 -0500

    Delete APICORE directory

--- APICORE/Models/Usuario.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace APICORE.Models
-{
-    public class Usuario
-    {
-        public int IdUsuario { get; set; }
-        public string Correo { get; set; }
-        public string Clave { get; set; }
-    }
-}
